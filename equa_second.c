bfb760c72a709817ad2f3ae49f19f5fba37b0185
MagnoQueiroz <synxther@protonmail.com>
2024-03-08 14:07:56 -0300

    add void function for clear screen in linux and windows

--- equa_second.c
+++ equa_second.c
@@ -1,62 +1,112 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+#include <stdlib.h>
+
+void clear_screen()
+{
+
+#ifdef __linux__
+    printf("\nPRESSIONE QUALQUER TECLA PARA CONTINUAR O PROCEDIMENTO\n");
+    char press = getchar();
+    system("clear");
+#elif _WIN32
+    printf("\nPRESSIONE QUALQUER TECLA PARA CONTINUAR O PROCEDIMENTO\n");
+    char press = getchar();
+    system("cls");
+#else
+
+#endif
+}
 
 int main()
 {
     setlocale(LC_ALL, "portuguese");
+
+    int repeat = 1, q;
     float xQua, xNum, num;
     float delta, raiz, xNumPow;
     float xOne, xTwo;
 
-    printf("|---------------------------------|\n");
-    printf("|          Equação do 2º          |\n");
-    printf("|---------------------------------|\n");
-    printf("|\t ax² + bx + c = 0         |\n");
-    printf("|---------------------------------|\n");
-
+    while (repeat == 1)
     {
-        printf("Digite a: ");
-        scanf("%f", &xQua);
-
-        if (xQua == 0)
+        char press;
+        printf("\n\t|-----------------------|");
+        printf("\n\t|\tBEM-VINDO !!\t|\n");
+        printf("\t|-----------------------|\n\n");
+        printf("Calculadora de equação do 2º\n");
+        printf("Digite:\n1- para iniciar programa\n2-para sair do programa\n");
+        printf("Escolha: ");
+        scanf("%d", &q);
+        if (q == 1)
         {
-            printf("A não pode ser zero. Digite um valor valido\n");
+
+            printf("|---------------------------------|\n");
+            printf("|          Equação do 2º          |\n");
+            printf("|---------------------------------|\n");
+            printf("|\t ax² + bx + c = 0         |\n");
+            printf("|---------------------------------|\n");
+
+            do
+            {
+                printf("Digite a: ");
+                scanf("%f", &xQua);
+
+                if (xQua == 0)
+                {
+                    printf("A não pode ser zero. Digite um valor valido\n");
+                }
+
+            } while (xQua == 0);
+
+            printf("Digite b: ");
+            scanf("%f", &xNum);
+
+            printf("Digite c: ");
+            scanf("%f", &num);
+
+            xNumPow = pow(xNum, 2);
+
+            delta = xNumPow - (4 * (xQua * num));
+
+            printf("O delta é: %.0f\n\n", delta);
+            printf("\t   -(b) +- √Δ\n\t_________________\n\t      2×a\n\n");
+
+            if (delta < 0)
+            {
+                printf("Não existe raiz quadrada negativa no números raiz\n\n");
+                printf("Logo Não é possível passar deste ponto:\n\n");
+                printf("\t-(%.2f) +- √%.2f\n\t___________________\n\t         %.0f\n\n", xNum, delta, (2 * xQua));
+            }
+            else
+            {
+                raiz = sqrt(delta);
+                xOne = (-xNum + raiz) / (2 * xQua);
+                xTwo = (-xNum - raiz) / (2 * xQua);
+
+                printf("\t -(%.2f) +- √%.2f\n\t___________________\n\t       2×%.0f\n\n", xNum, delta, xQua);
+                printf("\t -(%.2f) +- %.2f\n\t___________________\n\t         %.0f\n\n", xNum, raiz, (2 * xQua));
+
+                printf("x¹: %.2f\n", xOne);
+                printf("x²: %.2f\n", xTwo);
+            }
+            scanf("%c", &press);
+
+            clear_screen();
         }
-        // do while é ao contrario ao invés dele verificar se é falso ele verifica se é verdadeiro... ele vai verificar do falso e dps verdadeiro
-    } while (xQua == 0);
+        else if (q == 2)
+        {
+            printf("\nSAINDO...\n\n");
+            scanf("%c", &press);
+            clear_screen();
 
-    printf("Digite b: ");
-    scanf("%f", &xNum);
-
-    printf("Digite c: ");
-    scanf("%f", &num);
-
-    xNumPow = pow(xNum, 2);
-
-    delta = xNumPow - (4 * (xQua * num));
-
-    printf("O delta é: %.0f\n\n", delta);
-    printf("\t   -(b) +- √Δ\n\t_________________\n\t      2×a\n\n");
-
-    if (delta < 0)
-    {
-        printf("Não existe raiz quadrada negativa no números raiz\n\n");
-        printf("Logo Não é possível passar deste ponto:\n\n");
-        printf("\t-(%.2f) +- √%.2f\n\t___________________\n\t         %.0f\n\n", xNum, delta, (2 * xQua));
-    }
-    else
-    {
-        raiz = sqrt(delta);
-        xOne = (-xNum + raiz) / (2 * xQua);
-        xTwo = (-xNum - raiz) / (2 * xQua);
-
-        printf("\t -(%.2f) +- √%.2f\n\t___________________\n\t       2×%.0f\n\n", xNum, delta, xQua);
-        printf("\t -(%.2f) +- %.2f\n\t___________________\n\t         %.0f\n\n", xNum, raiz, (2 * xQua));
-
-        printf("x¹: %.2f\n", xOne);
-        printf("x²: %.2f\n", xTwo);
+            repeat = 0;
+        }
+        else
+        {
+            printf("Valor invalido, tente novamente\n");
+        }
     }
 
-    return (0);
+    return 0;
 }
